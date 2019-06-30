2534fae5948f53916c2f5b34bbff89072cf59633
kuojustin96 <justinkuo3904@yahoo.com>
2019-06-29 19:32:26 -0500

    Created BAMainGameplayUI, added some test map related stuff

---
+++ Source/BornAnew/Public/BAMainGameplayUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BAMainGameplayUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BORNANEW_API UBAMainGameplayUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
